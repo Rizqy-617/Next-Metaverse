337e0dbeaca402b8294479215ba06aa59e07b910
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-03-06 15:04:11 +0700

    finish Insight section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { motion } from "framer-motion"
 import { TypingText, TitleText } from "../components"
@@ -14,67 +14,61 @@ const World = () => (
       viewport={{ once: "false", amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText
-        title="| People on the world"
-        textStyles="text-center"
-      />
+      <TypingText title="| People on the world" textStyles="text-center" />
       <TitleText
-        title={
-          <>
-            Track friends around you and invite them to play together in the
-            same world
-          </>
-        }
+        title={ <>
+          Track friends around you and invite them to play together in the same world
+        </>}
         textStyles="text-center"
       />
-      <motion.div
-        variants={fadeIn("up", "tween", 0.3, 1)}
+      <motion.div 
+        variants={fadeIn('up', 'tween', 0.3, 1)}
         className="relative mt-[68px] flex w-full h-[550px]"
       >
-        <img
+        <img 
           src="/map.png"
           alt="map"
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]" >
+          <img 
             src="people-01.png"
             alt="people"
             className="w-full h-full"
           />
-        </div>
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img
+        </div> 
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]" >
+          <img 
             src="people-02.png"
             alt="people"
             className="w-full h-full"
           />
-        </div>
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-          <img
+        </div> 
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]" >
+          <img 
             src="people-03.png"
             alt="people"
             className="w-full h-full"
           />
-        </div>
-        <div className="absolute bottom-20 left-20">
-          <img
+        </div> 
+        <div className="absolute bottom-20 left-20" >
+          <img 
             src="card-1.svg"
             alt="people"
             className="w-full h-full"
           />
-        </div>
-        <div className="absolute top-[-50px] left-[45%]">
-          <img
+        </div> 
+        <div className="absolute top-[-50px] left-[45%]" >
+          <img 
             src="card-2.svg"
             alt="people"
             className="w-full h-full"
           />
-        </div>
+        </div> 
       </motion.div>
     </motion.div>
   </section>
-)
+);
 
-export default World
+export default World;
